7bbc29b53a30cf335e61a811e1cd8cd18977ad74
Julian Ullrich <julian@ullrichmail.net>
2021-05-08 00:45:19 +0200

    gamestate to extra file

---
+++ gamestate.h
@@ -0,0 +1,12 @@
+
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+enum gameState {
+  GAME_IDLE, // in menu, doing nothing
+  GAME_RUNNING, // running simulation
+  GAME_STOPPED, // simulation stopped (via user or automatically)
+  GAME_PREPARED, // startState loaded, ready to start
+};
+
+#endif
